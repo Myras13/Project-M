42608e06930cd5fa4dea1e62af32dfe5420b70e6
Michał Marciniak <michal.marciniak.it@gmail.com>
2023-08-07 18:27:29 +0200

    docker: Created account for all host

--- .docker/build/db/init/01-users.sql
+++ .docker/build/db/init/01-users.sql
@@ -1,2 +1,5 @@
 CREATE USER 'brisingr_app'@'localhost' IDENTIFIED BY 'XLToALmdxZ';
 GRANT ALL PRIVILEGES ON *.* TO 'brisingr_app'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'brisingr_app'@'%' IDENTIFIED BY 'XLToALmdxZ';
+GRANT ALL PRIVILEGES ON *.* TO 'brisingr_app'@'%' WITH GRANT OPTION;
