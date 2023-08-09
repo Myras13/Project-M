b6a28a720b3cb2b486dc49491ae431e875c2ee8c
Michał Marciniak <michal.marciniak.it@gmail.com>
2023-08-09 03:28:00 +0200

    feat: Create migration for user table

---
+++ Brisingr/migrations/Version20230808221721.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230808221721 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(60) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(145) NOT NULL, refresh_id VARCHAR(128) DEFAULT NULL, created_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , updated_at DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , deleted_at DATETIME DEFAULT NULL --(DC2Type:datetime_immutable)
+        , is_verified BOOLEAN NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE user');
+    }
+}
